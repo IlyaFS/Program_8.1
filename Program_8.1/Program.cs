ee99c2963811ab810deb6e1ac27db3f9a451fab5
Илья Филиппов <filippov.i.s@gmail.com>
2021-12-05 17:05:43 +0300

    Добавьте файлы проекта.

---
+++ Program_8.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Program_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Илья\\YandexDisk\\_work_";
+
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Папки:");
+                string[] folder = Directory.GetDirectories(path); // папки
+
+                foreach (string a in folder)
+                {
+                    Console.WriteLine(a);
+
+                    string[] infolder = Directory.GetDirectories(a);
+
+                    foreach (string c in infolder)
+                    {
+                        Console.WriteLine(c);
+                    }
+                }
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(path); // файлы
+
+                foreach (string b in files)
+                {
+                    Console.WriteLine(b);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
